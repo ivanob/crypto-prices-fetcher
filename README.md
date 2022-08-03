f5bb54a907b96ae1d875223861ad03348b328a2f
Ivan Obeso <ivan.obeso1@gmail.com>
2022-08-03 13:43:19 +0200

    lambda is now calling the external service to get prices

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # crypto-prices-fetcher
 
-- [ ] Create project using monorepos
-- [ ] Create AWS lambda function in TS
-- [ ] Create DynamoDB instance via code
+- [X] Create project using monorepos
+- [X] Create AWS lambda function in TS
+- [X] Create DynamoDB instance via code
+- [X] Testing on lambdas: execute the lambda locally + unit testing
+
+
+# Overview
+The project aims to fetch periodically the current price of some determined cryptos and store in
+a database via AWS lambda. Then, a graphql server exposes this data so can be consumed by a frontend app.
+The frontend client queries custom queries over this data, so no REST APIs are used here.
+
--- apps/aws-cron-crypto-prices-fetcher/src/handler.ts
+++ apps/aws-cron-crypto-prices-fetcher/src/handler.ts
@@ -3,8 +3,45 @@ import {
   APIGatewayProxyEvent,
   Context 
 } from "aws-lambda";
+import {DynamoDB} from "aws-sdk";
+import {v4 as uuidv4} from "uuid";
+import fetch from "node-fetch"
+
+const client = new DynamoDB.DocumentClient();
+
+/**
+ * Coingecko exposes an API to ask the current price of some cryptos and tokens. Example:
+  curl -X 'GET' \
+  'https://api.coingecko.com/api/v3/simple/price?ids=cardano%2Cbitcoin&vs_currencies=usd' \
+  -H 'accept: application/json'
+ */
+
+const fetchCryptoCurrentData = async (cryptos: string, baseCurrency: string): Promise<any> => {
+  return await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptos}&vs_currencies=${baseCurrency}`)
+}
+
+const writePriceInDB = async (crypto: string, price: number, timestamp: number) => {
+  const params = {
+    TableName: 'crypto-prices',
+    Item: {
+      id: uuidv4(),
+      crypto,
+      price,
+      timestamp
+    }
+  };
+  await client.put(params).promise();
+}
 
 export const run = async (event: APIGatewayProxyEvent, context: Context) => {
-  const time = new Date();
-  console.log(`Your cron function "${context.functionName}" ran at ${time}`);
+  const cryptoInfo = await fetchCryptoCurrentData(
+    process.env.CRYPTO_NAMES,
+    process.env.CURRENCY_BASE
+  );
+  const currentTimestamp = Math.round(Date.now() / 1000)
+  const cryptoSymbols = Object.values(cryptoInfo) as string[];
+  cryptoSymbols.forEach(async (cryptoSymbol, idx) => {
+    await writePriceInDB(cryptoSymbol, cryptoInfo[idx], currentTimestamp);
+  });
+  console.log('END')
 };
